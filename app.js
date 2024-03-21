f79fd65c8d668fd7cec9c94fad3a9f6c210a04e3
isaiah <isaiahpape@gmail.com>
2024-03-21 13:50:05 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+//Variables and arrays start here
+
+const heroes = [
+    {
+        name: 'Slate Slabrock',
+        type: 'dwarf',
+        damage: 5,
+        health: 100
+    },
+    {
+        name: 'Flint Ironstag',
+        type: 'elf',
+        damage: 10,
+        health: 50
+    }
+]
+
+const boss = {
+    health: 100,
+    maxHealth: 100,
+    damage: 5,
+    level: 1
+}
+
+//Variables and arrays end here
+//Code Starts here
+
+
+
+
+
+//Code Ends here
+//Functions start here
+
+
+
+
+
+//Functions end here
